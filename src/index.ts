48b659982bd6ff5b19b3742f97de36615a6819c5
mazer <mazer0701@gmail.com>
2025-06-14 17:54:18 +0800

    fix index.ts not export all class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { Item, User } from './core/entities';
 export { EmbeddingProcessor } from './core/preProcessor';
+export { ItemRegistry } from './core/registry';
 export { BetaLikelihoodReranker } from './core/reranker';
+export { betaPDF } from './core/statistics';
+export { UIController } from './core/uiController';
